e2f8319e01f6863e9a877a4948b5fb4179d53a5b
Paresh Zanzarukiya <zxparesh83@gmail.com>
2016-08-22 16:19:54 +0530

    minor changes

--- new_simulation/clear.sh
+++ new_simulation/clear.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-rm *.txt *.tmp *.png
+# Usage:
+# no arg -> delte all generated files
+# any arg -> delete only drawgraph.sh generated files
+
+if [ $# -eq 0 ];
+then
+	rm *.txt *.tmp *.png
+else
+	rm *.tmp *.png
+fi
--- new_simulation/convergence.py
+++ new_simulation/convergence.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python
 
-# You can call this function with:
-# arg1: ground_truth file (format: time soc)
-# arg2: soc experimental file (format: time soc)
+import sys
 
-def findConvergenceTime(file1, file2):
+# arg1: ground_truth file
+# arg2: soc experimental file
+# prints avg convergence time
 
-	fin1_pt = open(fin1, 'r')
-	fin2_pt = open(fin2, 'r')
-	# fout_pt = open(fout, 'w')
+fin1 = sys.argv[1]
+fin2 = sys.argv[2]
 
-	soc = int(-1)
-	con_time = 0
-	con_count = 0
+fin1_pt = open(fin1, 'r')
+fin2_pt = open(fin2, 'r')
+
+soc = int(-1)
+con_time = 0
+con_count = 0
 	
-	for line in fin1_pt:
-		line = line.split();
-		ttime = float(line[0])
-		tsoc = int(float(line[1]))
-		if (tsoc != soc):
-			for nline in fin2_pt:
-				nline = nline.split();
-				nsoc = int(float(nline[1]))
-				ntime = float(nline[0])
-				if (ntime>=ttime and nsoc == tsoc):
-					con_count += 1
-					con_time += ntime-ttime
-					break;
-			soc = tsoc
+for line in fin1_pt:
+	line = line.split();
+	ttime = float(line[0])
+	tsoc = int(float(line[1]))
+	if (tsoc != soc):
+		for nline in fin2_pt:
+			nline = nline.split();
+			nsoc = int(float(nline[1]))
+			ntime = float(nline[0])
+			if (ntime>=ttime and nsoc == tsoc):
+				con_count += 1
+				con_time += ntime-ttime
+				break;
+		soc = tsoc
 
-	return float(con_time/con_count)
+print float(con_time/con_count)
